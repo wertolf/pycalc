8b39ca904710ac68c5b361201ef28f5b2d9b0134
Yuquan Chi <32032140+wertolf@users.noreply.github.com>
2024-07-17 09:04:35 +0800

    Completing the View

--- pycalc.py
+++ pycalc.py
@@ -2,9 +2,20 @@
 
 import sys
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QApplication,
+    QGridLayout,
+    QLineEdit,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
 
 WINDOW_SIZE = 235
+DISPLAY_HEIGHT = 35
+BUTTON_SIZE = 40
 
 class PyCalcWindow(QMainWindow):
     """PyCalc's main window (GUI or view)."""
@@ -13,8 +24,57 @@ class PyCalcWindow(QMainWindow):
         super().__init__(parent=None)
         self.setWindowTitle("PyCalc")
         self.setFixedSize(WINDOW_SIZE, WINDOW_SIZE)
+        self.generalLayout = QVBoxLayout()
         centralWidget = QWidget(parent=self)
+        centralWidget.setLayout(self.generalLayout)
         self.setCentralWidget(centralWidget)
+        self._createDisplay()
+        self._createButtons()
+    
+    def _createDisplay(self):
+        self.display = QLineEdit()
+        self.display.setFixedHeight(DISPLAY_HEIGHT)
+        self.display.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.display.setReadOnly(True)
+        self.generalLayout.addWidget(self.display)
+    
+    def _createButtons(self):
+        self.buttonMap = {}
+        buttonsLayout = QGridLayout()
+        keyBoard = [
+            ["7", "8", "9", "/", "C"],
+            ["4", "5", "6", "*", "("],
+            ["1", "2", "3", "-", ")"],
+            ["0", "00", ".", "+", "="],
+        ]
+
+        for row, keys in enumerate(keyBoard):
+            for col, key in enumerate(keys):
+
+                # create the button
+                button = QPushButton(text=key)
+                button.setFixedSize(BUTTON_SIZE, BUTTON_SIZE)
+
+                # add the button to buttonsLayout
+                buttonsLayout.addWidget(button, row, col)
+
+                # add the button to self.buttonMap
+                self.buttonMap[key] = button
+
+        self.generalLayout.addLayout(buttonsLayout)
+
+    def setDisplayText(self, text):
+        """Set the display's text."""
+        self.display.setText(text)
+        self.display.setFocus()
+    
+    def getDisplayText(self):
+        """Get the display's text."""
+        return self.display.text()
+
+    def clearDisplay(self):
+        """Clear the display."""
+        self.setDisplayText("")
 
 def main():
     """PyCalc's main function."""
